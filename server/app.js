6e4137d3de1209546a0317da6d9c724592085295
etteryand0 <etteryand@gmail.com>
2021-03-25 13:13:50 +0900

    :bug: Handle CORS errors. Fixes #1

--- server/app.js
+++ server/app.js
@@ -6,7 +6,22 @@ const productRoutes = require('./api/routes/products');
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json())
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost');
+  res.header(
+    'Access-Control-Allow-Headers', 
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+
+  if (req.method == 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'POST, GET, DELETE');
+    return res.status(200).json({});
+  }
+
+  next();
+})
 
 app.use('/products', productRoutes);
 
