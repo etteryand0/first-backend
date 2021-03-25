21623c3f9bc69f7869a34c52434258f12c57e149
etteryand0 <etteryand@gmail.com>
2021-03-25 12:52:18 +0900

    :arrow_up: Error 404 handler

--- server/app.js
+++ server/app.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+
 app.use('/products', productRoutes);
+
+
+// Handle 404 error
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status(404);
+  next(error);
+})
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  })
+})
 
 module.exports = app;
