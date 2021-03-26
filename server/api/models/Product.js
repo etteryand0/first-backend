ad22c66560d3171f1344b83a183fa65dd5ca4610
etteryand0 <etteryand@gmail.com>
2021-03-26 17:01:55 +0900

    :lock: Limit Product.price so it won't be negative. Fixes #6

--- server/api/models/Product.js
+++ server/api/models/Product.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const ProductSchema = mongoose.Schema({
   _id: mongoose.SchemaTypes.ObjectId,
   name: { type: String, required: true, maxLength: 50},
-  price: { type: Number, required: true },
+  price: { type: Number, required: true, min: 0 },
 });
 
 module.exports = mongoose.model('Product', ProductSchema);
