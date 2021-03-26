d9746f8f159f0bbe84f2085b77abfc3bfe89b8ba
etteryand0 <etteryand@gmail.com>
2021-03-26 16:56:33 +0900

    :lock: Limit Product.name length. Fixes #4

--- server/api/models/Product.js
+++ server/api/models/Product.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const ProductSchema = mongoose.Schema({
   _id: mongoose.SchemaTypes.ObjectId,
-  name: { type: String, required: true },
+  name: { type: String, required: true, maxLength: 50},
   price: { type: Number, required: true },
 });
 
